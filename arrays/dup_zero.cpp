c47708b2f523b7e75588bb1786d02ab20fda03bb
Samarth_Ahuja <samahuja642@gmail.com>
2022-01-13 00:39:01 +0530

    Update dup_zero.cpp
    Writing Stuff for dup_zero.cpp

--- arrays/dup_zero.cpp
+++ arrays/dup_zero.cpp
@@ -1,50 +1,118 @@
-#include<bits/stdc++.h>
-using namespace std;
+// Attempt # 1
+// This Basic Brute Force Approach I am just traversing through the list and checking if the element is 0, if the element is zero adding another element into it with value 0.
+// As we Want same sized array then we should pop one value from last so that our array size is maintained.
+// Question 1 : What is the complexity of accessing itertor as (arr.begin() + N) ? 
+// Complexity Analysis 
+// Space Complexity -> O(1)
+// Time Complexity -> O(N^2) or O(N^3) depends on the question which i mentioned above , but surely we can do it using iterators in O(N^2)
+
+// Code
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        int start = 0;
-        int end = arr.size()-1;
-        int zeroes = 0;
-        // cout<<start<<endl;
-        // cout<<end<<endl;
-        while(start<=end){
-            if(arr[start]==0){end--;zeroes++;
-            // cout<<start<<" "<<end;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==0){
+                arr.insert(arr.begin()+i,0);
+                arr.pop_back();
+                i++;
             }
-            // cout<<start<<" "<<end<<endl;
-            start++;
         }
-        int non_zeroes = start - zeroes;
-        start--;
-        for(int i=arr.size()-1;i>=0;i--){
-            if(i==arr.size()-1 && non_zeroes+2*zeroes>arr.size()){
-                arr[i]=arr[start];
-            }
-            else if(arr[start]==0){
-                arr[i]=0;
-                i--;
-                arr[i]=0;
-            }
-            else{
-                arr[i]=arr[start];
-            }
-            start--;
-        }
-        // cout<<zeroes<<endl;
-        // cout<<non_zeroes<<endl;
-        // cout<<arr.size()<<endl;
-        // cout<<end<<endl;
-        // cout<<start<<endl;
     }
 };
-int main(){
-    Solution * obj = new Solution();
-    vector<int> arr = {1,5,2,0,6,8,0,6,0};
-    obj->duplicateZeros(arr);
-    for(int i=0;i<arr.size();i++){
-        cout<<arr[i]<<" ";	
-    }
-    cout<<endl;
-    return 0;
-}
+
+// Attempt # 2
+// Two Pointer Approach
+// Basically we have taken another array and storing the values into it.
+// Then left and right are assigned their respective values.
+// So if value at left is 0 then we have to reduce the value of right as to maintain size and left is always increasing.
+// But take care for the corner cases when left is equal to right that means there is only one space left for another element in this case value can be anything but it will be pushed only one time.
+// Then we are just coping the values from temporary array to our array and its done.
+// Complexity Analysis 
+// Space Complexity -> O(N)
+// Time Complexity -> O(N)
+
+// Code
+// class Solution {
+// public:
+//     void duplicateZeros(vector<int>& arr) {
+//         vector<int> temp;
+//         int left = 0;
+//         int right = arr.size() - 1;
+//         while(left<=right){
+//             if(left==right){
+//                 temp.push_back(arr[left]);
+//                 left++;
+//             }
+//             else{
+//                 if(arr[left]==0){
+//                     temp.push_back(0);
+//                     temp.push_back(0);
+//                     left++;
+//                     right--;
+//                 }           
+//                 else{
+//                     temp.push_back(arr[left]);
+//                     left++;
+//                 }
+//             }
+//         }
+//         for(int i=0;i<arr.size();i++){
+//             arr[i]=temp[i];
+//         }
+//     }
+// };
+
+
+// When I was Solving it Random.
+
+//Code
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Solution {
+// public:
+//     void duplicateZeros(vector<int>& arr) {
+//         int start = 0;
+//         int end = arr.size()-1;
+//         int zeroes = 0;
+//         // cout<<start<<endl;
+//         // cout<<end<<endl;
+//         while(start<=end){
+//             if(arr[start]==0){end--;zeroes++;
+//             // cout<<start<<" "<<end;
+//             }
+//             // cout<<start<<" "<<end<<endl;
+//             start++;
+//         }
+//         int non_zeroes = start - zeroes;
+//         start--;
+//         for(int i=arr.size()-1;i>=0;i--){
+//             if(i==arr.size()-1 && non_zeroes+2*zeroes>arr.size()){
+//                 arr[i]=arr[start];
+//             }
+//             else if(arr[start]==0){
+//                 arr[i]=0;
+//                 i--;
+//                 arr[i]=0;
+//             }
+//             else{
+//                 arr[i]=arr[start];
+//             }
+//             start--;
+//         }
+//         // cout<<zeroes<<endl;
+//         // cout<<non_zeroes<<endl;
+//         // cout<<arr.size()<<endl;
+//         // cout<<end<<endl;
+//         // cout<<start<<endl;
+//     }
+// };
+// int main(){
+//     Solution * obj = new Solution();
+//     vector<int> arr = {1,5,2,0,6,8,0,6,0};
+//     obj->duplicateZeros(arr);
+//     for(int i=0;i<arr.size();i++){
+//         cout<<arr[i]<<" ";	
+//     }
+//     cout<<endl;
+//     return 0;
+// }
