8eff06adc6d11ac57c5b6c434fd2e000af28c94d
Samarth_Ahuja <samahuja642@gmail.com>
2022-01-12 21:11:31 +0530

    Update maximum_consecutive_ones.cpp
    Writing All the Stuff for maximum consecutive ones

--- arrays/maximum_consecutive_ones.cpp
+++ arrays/maximum_consecutive_ones.cpp
@@ -1,3 +1,12 @@
+// Attempt #1
+// Traversing through the array and when we encounter 1 we enter the loop and traverse ahead with counting the number of ones till we encounter 0.
+// Then returning the max value from the all will be our answer.
+// Question #1 :- I think this solution is quite good but still it beats only 8% of people what i have done Wrong!! Its Complexity is fine i guess.
+// Complexity Analysis
+// Space Complexity -> O(1)
+// Time Complexity -> O(N)
+// As many of time if we see two loops we be like its O(N^2) , but you should check with copy pen using some math and my little brain that how many time an operation occured.
+
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
