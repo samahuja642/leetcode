6e2fdd67155d375bc896d91dcb17f0d60f1d9fe0
Samarth_Ahuja <samahuja642@gmail.com>
2022-02-02 19:03:27 +0530

    Update kthSymbolGrammar.cpp

--- recursion/kthSymbolGrammar.cpp
+++ recursion/kthSymbolGrammar.cpp
@@ -1,3 +1,12 @@
+// Attempt # 1
+// Simple Recursive Solution
+// Observation was important than Recursion
+// 1 -> 0
+// 2 -> 0 1
+// 3 -> 0 1 1 0
+// 4 -> 0 1 1 0 1 0 0 1
+
+// Code
 class Solution {
 public:
     int kthGrammar(int n, int k) {
