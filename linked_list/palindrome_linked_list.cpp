1612af0a12bf4f289d7c2e64321b48fac1b5d9f1
Samarth_Ahuja <samahuja642@gmail.com>
2022-02-09 21:57:23 +0530

    Update palindrome_linked_list.cpp

--- linked_list/palindrome_linked_list.cpp
+++ linked_list/palindrome_linked_list.cpp
@@ -20,3 +20,47 @@ public:
         return false;
     }
 };
+
+// Attempt # 2
+// spliting in half and reverse second half 
+// then compare both
+// Complexity Analysis
+// Space Complexity -> O(1)
+// Time Complexity -> O(N)
+
+// Code
+class Solution {
+public: 
+    ListNode* reverse_list(ListNode* head){
+        ListNode* prev = NULL;
+        while(head->next!=NULL){
+            ListNode* temp = head->next;
+            head->next = prev; 
+            prev = head;
+            head = temp;
+        }
+        head->next = prev;
+        return head;
+    }
+    bool isPalindrome(ListNode* head) {
+        int size = 0;
+        ListNode* temp = head;
+        while(temp!=NULL){
+            temp=temp->next;
+            size++;
+        }
+        if(size==1)return true;
+        int index = size%2==0?size/2:size/2 + 1;
+        temp = head;
+        for(int i=0;i<index;i++){
+            temp = temp->next;
+        }
+        temp = reverse_list(temp);
+        while(temp!=NULL){
+            if(head->val!=temp->val)return false;
+            head = head->next;
+            temp = temp->next;
+        }
+        return true;
+    }
+};
